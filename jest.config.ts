8893bc9e9a9083f5f257921186e7387acc126ee5
Luan Eduardo da Costa <luan1346@gmail.com>
2021-09-01 21:35:09 -0300

    Fix jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
 export default {
-  rootDir: 'src',
   clearMocks: true,
   testTimeout: 30000,
   collectCoverage: true,
   coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
 }

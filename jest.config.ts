3d79a22069905b816a8c45037627a4882784c368
Luan Eduardo da Costa <luan1346@gmail.com>
2021-09-01 23:33:56 -0300

    Add types to jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,13 @@
-export default {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
   clearMocks: true,
   testTimeout: 30000,
   collectCoverage: true,
   coverageProvider: 'v8',
   roots: ['<rootDir>/src'],
+  testEnvironment: 'jsdom',
   coverageDirectory: 'coverage',
 }
+
+export default config

2de00f2cfda2e4775e88711470d9bf37dbc43854
Luan Eduardo da Costa <luan1346@gmail.com>
2021-09-06 15:08:38 -0300

    Transpile code to ES6 instead of ES5

--- dist/index.common.js
+++ dist/index.common.js
@@ -11,46 +11,11 @@ function __awaiter(thisArg, _arguments, P, generator) {
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 }
-function __generator(thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-}
 
 function getBlobFromImageSource(imageSource) {
-    return __awaiter(this, void 0, void 0, function () {
-        var response;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4, fetch("" + imageSource)];
-                case 1:
-                    response = _a.sent();
-                    return [4, response.blob()];
-                case 2: return [2, _a.sent()];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch(`${imageSource}`);
+        return yield response.blob();
     });
 }
 function isJpegBlob(blob) {
@@ -60,89 +25,65 @@ function isPngBlob(blob) {
     return blob.type.includes('png');
 }
 function createImageElement(imageSource) {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            return [2, new Promise(function (resolve, reject) {
-                    var imageElement = document.createElement('img');
-                    imageElement.crossOrigin = 'anonymous';
-                    imageElement.src = imageSource;
-                    imageElement.onload = function (event) {
-                        var target = event.target;
-                        resolve(target);
-                    };
-                    imageElement.onabort = reject;
-                    imageElement.onerror = reject;
-                })];
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise(function (resolve, reject) {
+            const imageElement = document.createElement('img');
+            imageElement.crossOrigin = 'anonymous';
+            imageElement.src = imageSource;
+            imageElement.onload = function (event) {
+                const target = event.target;
+                resolve(target);
+            };
+            imageElement.onabort = reject;
+            imageElement.onerror = reject;
         });
     });
 }
 function getBlobFromImageElement(imageElement) {
-    return __awaiter(this, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            return [2, new Promise(function (resolve, reject) {
-                    var canvas = document.createElement('canvas');
-                    var context = canvas.getContext('2d');
-                    if (context) {
-                        var width = imageElement.width, height = imageElement.height;
-                        canvas.width = width;
-                        canvas.height = height;
-                        context.drawImage(imageElement, 0, 0, width, height);
-                        canvas.toBlob(function (blob) {
-                            if (blob)
-                                resolve(blob);
-                            else
-                                reject('Cannot get blob from image element');
-                        }, 'image/png', 1);
-                    }
-                })];
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise(function (resolve, reject) {
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+            if (context) {
+                const { width, height } = imageElement;
+                canvas.width = width;
+                canvas.height = height;
+                context.drawImage(imageElement, 0, 0, width, height);
+                canvas.toBlob(function (blob) {
+                    if (blob)
+                        resolve(blob);
+                    else
+                        reject('Cannot get blob from image element');
+                }, 'image/png', 1);
+            }
         });
     });
 }
 function convertBlobToPng(imageBlob) {
-    return __awaiter(this, void 0, void 0, function () {
-        var imageSource, imageElement;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    imageSource = URL.createObjectURL(imageBlob);
-                    return [4, createImageElement(imageSource)];
-                case 1:
-                    imageElement = _a.sent();
-                    return [4, getBlobFromImageElement(imageElement)];
-                case 2: return [2, _a.sent()];
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        const imageSource = URL.createObjectURL(imageBlob);
+        const imageElement = yield createImageElement(imageSource);
+        return yield getBlobFromImageElement(imageElement);
     });
 }
 function copyBlobToClipboard(blob) {
-    var _a;
-    var items = (_a = {}, _a[blob.type] = blob, _a);
-    var clipboardItem = new ClipboardItem(items);
+    const items = { [blob.type]: blob };
+    const clipboardItem = new ClipboardItem(items);
     navigator.clipboard.write([clipboardItem]);
 }
 function copyImageToClipboard(imageSource) {
-    return __awaiter(this, void 0, void 0, function () {
-        var blob, pngBlob;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4, getBlobFromImageSource(imageSource)];
-                case 1:
-                    blob = _a.sent();
-                    if (!isJpegBlob(blob)) return [3, 3];
-                    return [4, convertBlobToPng(blob)];
-                case 2:
-                    pngBlob = _a.sent();
-                    copyBlobToClipboard(pngBlob);
-                    return [2];
-                case 3:
-                    if (isPngBlob(blob)) {
-                        copyBlobToClipboard(blob);
-                        return [2];
-                    }
-                    _a.label = 4;
-                case 4: throw new Error('Cannot copy this type of image to clipboard');
-            }
-        });
+    return __awaiter(this, void 0, void 0, function* () {
+        const blob = yield getBlobFromImageSource(imageSource);
+        if (isJpegBlob(blob)) {
+            const pngBlob = yield convertBlobToPng(blob);
+            copyBlobToClipboard(pngBlob);
+            return;
+        }
+        else if (isPngBlob(blob)) {
+            copyBlobToClipboard(blob);
+            return;
+        }
+        throw new Error('Cannot copy this type of image to clipboard');
     });
 }
 

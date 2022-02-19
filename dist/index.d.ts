22456401acc85f019160822a993a0c5d7960736f
Luan Eduardo da Costa <luan1346@gmail.com>
2022-02-19 17:57:40 -0300

    Write jsdoc comments to improve the documentation (#25)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,79 @@
+/**
+ * Gets a blob from an image source attribute using the Fetch API.
+ *
+ * @param {string} imageSource The image source attribute.
+ * @returns {Promise<Blob>} A promise that resolves to a image blob.
+ */
 export declare function getBlobFromImageSource(imageSource: string): Promise<Blob>;
+/**
+ * Checks if is a JPEG image blob.
+ *
+ * @param {Blob} blob A blob.
+ * @returns {boolean} A boolean indicating if the blob is a JPEG image or not.
+ */
 export declare function isJpegBlob(blob: Blob): boolean;
+/**
+ * Checks if is a PNG image blob.
+ *
+ * @param {Blob} blob A blob.
+ * @returns {boolean} A boolean indicating if the blob is a PNG image or not.
+ */
 export declare function isPngBlob(blob: Blob): boolean;
+/**
+ * Created an image element for a given image source attribute.
+ *
+ * @param {string} imageSource The image source attribute.
+ * @returns {Promise<HTMLImageElement>} A promise that resolves to an image element. Rejects the promise if cannot create an image element.
+ */
 export declare function createImageElement(imageSource: string): Promise<HTMLImageElement>;
+/**
+ * Gets a blob from an image element.
+ *
+ * @param {HTMLImageElement} imageElement An image element
+ * @returns {Promise<Blob>} A Promise that resolves to a image blob. Rejects the promise if cannot get a blob from the image element.
+ */
 export declare function getBlobFromImageElement(imageElement: HTMLImageElement): Promise<Blob>;
+/**
+ * Converts a JPEG image blob to PNG.
+ *
+ * @param {Blob} imageBlob JPEG blob that will be converted to PNG.
+ * @returns {Promise<Blob>} A Promise that resolves to a PNG image blob. Rejects the promise if cannot create an image element or if cannot get a blob from the image element.
+ */
 export declare function convertBlobToPng(imageBlob: Blob): Promise<Blob>;
+/**
+ * Copies a blob to user's clipboard.
+ *
+ * Throws an error if cannot write on the user's clipboard.
+ *
+ * @param {Blob} blob Blob to be copied.
+ */
 export declare function copyBlobToClipboard(blob: Blob): Promise<void>;
+/**
+ * Copies a PNG or JPEG image to clipboard.
+ *
+ * This function downloads the image to copy with it's original dimensions.
+ *
+ * - If the image is JPEG it will be converted automatically to PNG and then copied.
+ * - If the image is not PNG or JPEG an error will be thrown.
+ *
+ * @param {string} imageSource The image source attribute.
+ * @returns {Promise<Blob>} A promise that resolves to a blob. Generally you don't need to use the returned blob for nothing.
+ */
 export declare function copyImageToClipboard(imageSource: string): Promise<Blob>;
+/**
+ * Requests the permission to write data on the user's clipboard.
+ *
+ * Reasons why you generally don't need to use this function:
+ *
+ * - The Permission to write data on the clipboard is automatically granted to pages when they are in the browser active tab.
+ * - If the browser has not implemented the Permissions API yet, this function will return false.
+ *
+ * @returns {Promise<boolean>} A Promise that resolves to a boolean indicating if the permission was granted or not.
+ */
 export declare function requestClipboardWritePermission(): Promise<boolean>;
+/**
+ * Checks if can copy images to the clipboard using the Fetch API and the Clipboard API.
+ *
+ * @returns {Boolean} A boolean indicating if can copy or not.
+ */
 export declare function canCopyImagesToClipboard(): boolean;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Gets a blob from an image source attribute using the Fetch API.
+ *
+ * @param {string} imageSource The image source attribute.
+ * @returns {Promise<Blob>} A promise that resolves to a image blob.
+ */
 export async function getBlobFromImageSource(
   imageSource: string,
 ): Promise<Blob> {
@@ -5,14 +11,32 @@ export async function getBlobFromImageSource(
   return await response.blob()
 }
 
+/**
+ * Checks if is a JPEG image blob.
+ *
+ * @param {Blob} blob A blob.
+ * @returns {boolean} A boolean indicating if the blob is a JPEG image or not.
+ */
 export function isJpegBlob(blob: Blob): boolean {
   return blob.type.includes('jpeg')
 }
 
+/**
+ * Checks if is a PNG image blob.
+ *
+ * @param {Blob} blob A blob.
+ * @returns {boolean} A boolean indicating if the blob is a PNG image or not.
+ */
 export function isPngBlob(blob: Blob): boolean {
   return blob.type.includes('png')
 }
 
+/**
+ * Created an image element for a given image source attribute.
+ *
+ * @param {string} imageSource The image source attribute.
+ * @returns {Promise<HTMLImageElement>} A promise that resolves to an image element. Rejects the promise if cannot create an image element.
+ */
 export async function createImageElement(
   imageSource: string,
 ): Promise<HTMLImageElement> {
@@ -31,6 +55,12 @@ export async function createImageElement(
   })
 }
 
+/**
+ * Gets a blob from an image element.
+ *
+ * @param {HTMLImageElement} imageElement An image element
+ * @returns {Promise<Blob>} A Promise that resolves to a image blob. Rejects the promise if cannot get a blob from the image element.
+ */
 export async function getBlobFromImageElement(
   imageElement: HTMLImageElement,
 ): Promise<Blob> {
@@ -56,12 +86,25 @@ export async function getBlobFromImageElement(
   })
 }
 
+/**
+ * Converts a JPEG image blob to PNG.
+ *
+ * @param {Blob} imageBlob JPEG blob that will be converted to PNG.
+ * @returns {Promise<Blob>} A Promise that resolves to a PNG image blob. Rejects the promise if cannot create an image element or if cannot get a blob from the image element.
+ */
 export async function convertBlobToPng(imageBlob: Blob): Promise<Blob> {
   const imageSource = URL.createObjectURL(imageBlob)
   const imageElement = await createImageElement(imageSource)
   return await getBlobFromImageElement(imageElement)
 }
 
+/**
+ * Copies a blob to user's clipboard.
+ *
+ * Throws an error if cannot write on the user's clipboard.
+ *
+ * @param {Blob} blob Blob to be copied.
+ */
 export async function copyBlobToClipboard(blob: Blob): Promise<void> {
   const items = { [blob.type]: blob } as unknown as Record<
     string,
@@ -72,6 +115,17 @@ export async function copyBlobToClipboard(blob: Blob): Promise<void> {
   await navigator.clipboard.write([clipboardItem])
 }
 
+/**
+ * Copies a PNG or JPEG image to clipboard.
+ *
+ * This function downloads the image to copy with it's original dimensions.
+ *
+ * - If the image is JPEG it will be converted automatically to PNG and then copied.
+ * - If the image is not PNG or JPEG an error will be thrown.
+ *
+ * @param {string} imageSource The image source attribute.
+ * @returns {Promise<Blob>} A promise that resolves to a blob. Generally you don't need to use the returned blob for nothing.
+ */
 export async function copyImageToClipboard(imageSource: string): Promise<Blob> {
   const blob = await getBlobFromImageSource(imageSource)
 
@@ -87,6 +141,16 @@ export async function copyImageToClipboard(imageSource: string): Promise<Blob> {
   throw new Error('Cannot copy this type of image to clipboard')
 }
 
+/**
+ * Requests the permission to write data on the user's clipboard.
+ *
+ * Reasons why you generally don't need to use this function:
+ *
+ * - The Permission to write data on the clipboard is automatically granted to pages when they are in the browser active tab.
+ * - If the browser has not implemented the Permissions API yet, this function will return false.
+ *
+ * @returns {Promise<boolean>} A Promise that resolves to a boolean indicating if the permission was granted or not.
+ */
 export async function requestClipboardWritePermission(): Promise<boolean> {
   if (!navigator?.permissions?.query) return false
 
@@ -97,6 +161,11 @@ export async function requestClipboardWritePermission(): Promise<boolean> {
   return state === 'granted'
 }
 
+/**
+ * Checks if can copy images to the clipboard using the Fetch API and the Clipboard API.
+ *
+ * @returns {Boolean} A boolean indicating if can copy or not.
+ */
 export function canCopyImagesToClipboard(): boolean {
   const hasFetch = typeof fetch !== 'undefined'
   const hasClipboardItem = typeof ClipboardItem !== 'undefined'
